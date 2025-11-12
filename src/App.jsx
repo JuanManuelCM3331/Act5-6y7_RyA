3bed068ad332176987574562813a358a3735d5ab
juanma3331 <juanma3331@gmail.com>
2025-11-12 00:14:24 -0500

    feat(app): integrate Info, Habilidades, Education and Footer components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,49 @@
-import Header from './componentes/Header'
-import Info from './componentes/Info'
-import Educacion from './componentes/Educacion'
-import Experiencia from './componentes/Experiencia'
-import Habilidades from './componentes/Habilidades'
-import Footer from './componentes/Footer'
+import Header from "./componentes/Header";
+import Info from "./componentes/Info";
+import Educacion from "./componentes/Educacion";
+import Experiencia from "./componentes/Experiencia";
+import Habilidades from "./componentes/Habilidades";
+import Footer from "./componentes/Footer";
 
 function App() {
   return (
     <div className="container">
-      <Header />
+      <Header nombre="Juan M. Cardona M." cargo="Desarrollador Frontend" />
       <main>
-        <Info />
-        <Educacion />
-        <Experiencia />
-        <Habilidades />
+        <Info
+          nombre="Juan M. Cardona M."
+          cargo="Desarrollador Frontend"
+          ubicacion="Itagui, Antioquia, Colombia"
+          descripcion="Apasionado por la tecnología, con experiencia en React, Node.js y desarrollo web moderno."
+        />
+        <Educacion
+          estudios={[
+            {
+              titulo: "Tecnólogo en Análisis y Desarrollo de Software",
+              institucion: "SENA",
+              año: "2025",
+            },
+            {
+              titulo: "Tecnica en Operacion de Eventos",
+              institucion: "SENA",
+              año: "2024",
+            },
+          ]}
+        />
+
+        <Experiencia empresa="N/A" cargo="Frontend Developer" años="N/A" />
+        <Habilidades
+          tecnologias={["React", "Node.js", "JavaScript", "HTML", "CSS"]}
+        />
       </main>
-      <Footer />
+      <Footer
+        email="juanma3331@gmail.com"
+        telefono="+57 312 864 7249"
+        github="https://github.com/JuanManuelCM3331"
+        linkedin="https://www.linkedin.com/in/juan-manuel-cardona-molina-b0958a311/"
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
